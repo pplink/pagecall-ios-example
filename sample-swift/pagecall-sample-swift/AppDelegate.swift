444625279adc38faf23d0d8f95a847661389aed1
Sehun Park <defsehun@gmail.com>
2020-10-23 16:59:49 +0900

    Add exitWithBackup, clearFinishTimer with runJSFunction test code

--- sample-swift/pagecall-sample-swift/AppDelegate.swift
+++ sample-swift/pagecall-sample-swift/AppDelegate.swift
@@ -26,12 +26,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        PageCall.sharedInstance().runJSFunction("window['__exitWithBackup']()") { (result: Bool) in
+            if (result) {
+                NSLog("exitWithBackup success")
+            }
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        PageCall.sharedInstance().runJSFunction("window['__clearFinishTimer']()") { (result: Bool) in
+            if (result) {
+                NSLog("clearFinishTimer success")
+            }
+        }
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
